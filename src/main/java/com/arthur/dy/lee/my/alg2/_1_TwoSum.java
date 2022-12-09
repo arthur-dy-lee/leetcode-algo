a472bffc9135b557b828b47afe59986e7eedac54
arthur-dy-lee <arthur.dy.lee@gmail.com>
2022-12-09 10:12:30 +0800

    1

---
+++ src/main/java/com/arthur/dy/lee/my/alg2/_1_TwoSum.java
@@ -0,0 +1,36 @@
+package com.arthur.dy.lee.my.alg2;
+
+import com.alibaba.druid.support.json.JSONUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @TODO
+ */
+public class _1_TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        if (nums.length == 1) {
+            throw new IllegalArgumentException("not exist in nums");
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(nums[0], 0);
+        for (int i = 1; i < nums.length; i++) {
+            if (map.get(target - nums[i]) != null) {
+                return new int[] { map.get(target - nums[i]), i };
+            } else {
+                map.put(nums[i], i);
+            }
+
+        }
+        throw new IllegalArgumentException("not exist in nums");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 2, 4 };
+        System.out.println(JSONUtils.toJSONString(twoSum(nums, 6)));
+        int[] nums2 = { 2, 7, 11, 15 };
+        System.out.println(JSONUtils.toJSONString(twoSum(nums2, 9)));
+    }
+}
---
+++ src/main/java/com/arthur/dy/lee/my/alg2/_2_addTwoNumbers.java
@@ -0,0 +1,43 @@
+package com.arthur.dy.lee.my.alg2;
+
+import arthur.dy.lee.alg.common.GenListNode;
+import arthur.dy.lee.alg.common.ListNode;
+
+public class _2_addTwoNumbers {
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode curr = head;
+        int h = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+
+            int temp = x + y + h;
+            h = temp > 9 ? 1 : 0;
+            temp = temp % 10;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            curr.next = new ListNode(temp);
+            curr = curr.next;
+        }
+        if (h == 1) {
+            curr.next = new ListNode(h);
+        }
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = GenListNode.genNode9999999();
+        ListNode l2 = GenListNode.genNode9999();
+        ListNode ret = _2_addTwoNumbers.addTwoNumbers(l1, l2);
+        //        ListNode l1 = ListNode.buildListNode(new int[] { 2, 4, 6, 8 });
+        System.out.println(ret);
+    }
+
+}
