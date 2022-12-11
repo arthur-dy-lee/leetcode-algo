7fef2baed236755276036f50df776c485b2af56d
arthur-dy-lee <arthur.dy.lee@gmail.com>
2022-12-11 16:43:22 +0800

    1

---
+++ src/main/java/com/arthur/dy/lee/best/_11_maxArea.java
@@ -0,0 +1,41 @@
+package com.arthur.dy.lee.best;
+
+/**
+ * 11. 盛最多水的容器
+ * <p>
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ * <p>
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * <p>
+ * 返回容器可以储存的最大水量。
+ * <p>
+ * 说明：你不能倾斜容器。
+ */
+public class _11_maxArea {
+
+    public static int maxArea(int[] height) {
+        if (height.length < 2) {
+            return 0;
+        }
+        int max = 0;
+        int leftIndex = 0;
+        int rightIndex = height.length - 1;
+        while (leftIndex < rightIndex) {
+            int leftVal = height[leftIndex];
+            int rightVal = height[rightIndex];
+            max = Math.max(max, (rightIndex - leftIndex) * Math.min(leftVal, rightVal));
+            if (leftVal > rightVal) {
+                rightIndex--;
+            } else {
+                leftIndex++;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        System.out.println(_11_maxArea.maxArea(height));
+    }
+
+}
---
+++ src/main/java/com/arthur/dy/lee/best/_7_reverseInteger.java
@@ -0,0 +1,48 @@
+package com.arthur.dy.lee.best;
+
+/**
+ * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+ * <p>
+ * 如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+ * <p>
+ * 假设环境不允许存储 64 位整数（有符号或无符号）。
+ * <p>
+ * 输入：x = 123
+ * 输出：321
+ * <p>
+ * 输入：x = -123
+ * 输出：-321
+ * <p>
+ * 输入：x = 120
+ * 输出：21
+ * <p>
+ * 提示：
+ * <p>
+ * -231 <= x <= 231 - 1
+ */
+public class _7_reverseInteger {
+    public static int reverse(int x) {
+        int count = 0;
+        int result = 0;
+
+        while (x != 0){
+            result = result * 10 + x % 10;
+            count++;
+            x = x / 10;
+            if (count > 62) {
+                return 0;
+            }
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int x1 = 123;
+        _7_reverseInteger.reverse(x1);
+        int x2 = -123;
+        _7_reverseInteger.reverse(x2);
+        int x3 = 0;
+        _7_reverseInteger.reverse(x3);
+    }
+}
